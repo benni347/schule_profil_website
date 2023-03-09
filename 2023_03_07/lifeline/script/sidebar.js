cb1ebfddc5c200a2a76a5fbd7eddd904860a1cf6
Cédric Skwar <cdrc@skwar.me>
2023-03-09 20:15:40 +0100

    Added some small features.

--- 2023_03_07/lifeline/script/sidebar.js
+++ 2023_03_07/lifeline/script/sidebar.js
@@ -4,8 +4,26 @@ document.querySelector('.settings_cog').addEventListener('click', function() {
   if (selectors.contains("sidebar_closed")) {
     element.classList.remove("sidebar_closed");
     element.classList.add("sidebar_visible");
+    document.querySelector(".settings_hidden").classList.add("content_settings_visibile")
+
   } else {
     element.classList.remove("sidebar_visible");
     element.classList.add("sidebar_closed");
+    document.querySelector(".settings_hidden").classList.remove("content_settings_visibile")
+  }
+});
+
+document.querySelector('.close_button').addEventListener('click', function() {
+  let element = document.querySelector(".sidebar")
+  let selectors = element.classList
+  if (selectors.contains("sidebar_closed")) {
+    element.classList.remove("sidebar_closed");
+    element.classList.add("sidebar_visible");
+    document.querySelector(".settings_hidden").classList.add("content_settings_visibile")
+
+  } else {
+    element.classList.remove("sidebar_visible");
+    element.classList.add("sidebar_closed");
+    document.querySelector(".settings_hidden").classList.remove("content_settings_visibile")
   }
 });
