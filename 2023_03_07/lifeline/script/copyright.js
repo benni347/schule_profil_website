d76a2c791b3ae4ae2d18de40d9f908df2903bd81
Cédric Skwar <cdrc@skwar.me>
2023-03-09 20:50:51 +0100

    Added small changes

--- 2023_03_07/lifeline/script/copyright.js
+++ 2023_03_07/lifeline/script/copyright.js
@@ -1,4 +1,5 @@
 const year = new Date().getFullYear();
-const copyrightNotice = '&copy; Cédric Skwar - ' + year;
+const copyrightSymbol = String.fromCharCode(169);
+const copyrightNotice = `${copyrightSymbol} Cédric Skwar - ${year}`;
 const copyParagraph = document.querySelector('div.copy');
 copyParagraph.textContent = copyrightNotice;
