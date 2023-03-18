5999e6c917feee8dd4521d7bc2ad47b5ac59343f
Cédric Skwar <cdrc@skwar.me>
2023-03-18 12:00:35 +0100

    Started updating my page for the new desgin.

--- 2023_03_07/lifeline/script/sidebar.js
+++ 2023_03_07/lifeline/script/sidebar.js
@@ -1,35 +1,39 @@
-document.querySelector(".settings_cog").addEventListener("click", function () {
-    let element = document.querySelector(".sidebar")
-    let selectors = element.classList
-    if (selectors.contains("sidebar_closed")) {
-        element.classList.remove("sidebar_closed")
-        element.classList.add("sidebar_visible")
-        document
-            .querySelector(".settings_hidden")
-            .classList.add("content_settings_visibile")
-    } else {
-        element.classList.remove("sidebar_visible")
-        element.classList.add("sidebar_closed")
-        document
-            .querySelector(".settings_hidden")
-            .classList.remove("content_settings_visibile")
-    }
-})
+const closeButton = document.querySelector(".close_button");
 
-document.querySelector(".close_button").addEventListener("click", function () {
-    let element = document.querySelector(".sidebar")
-    let selectors = element.classList
-    if (selectors.contains("sidebar_closed")) {
-        element.classList.remove("sidebar_closed")
-        element.classList.add("sidebar_visible")
-        document
-            .querySelector(".settings_hidden")
-            .classList.add("content_settings_visibile")
-    } else {
-        element.classList.remove("sidebar_visible")
-        element.classList.add("sidebar_closed")
-        document
-            .querySelector(".settings_hidden")
-            .classList.remove("content_settings_visibile")
-    }
-})
+/*
+document.querySelector(".settings_cog").addEventListener("click", function () {
+  const element = document.querySelector(".sidebar");
+  const selectors = element.classList;
+  if (selectors.contains("sidebar_closed")) {
+    element.classList.remove("sidebar_closed");
+    element.classList.add("sidebar_visible");
+    document
+      .querySelector(".settings_hidden")
+      .classList.add("content_settings_visibile");
+  } else {
+    element.classList.remove("sidebar_visible");
+    element.classList.add("sidebar_closed");
+    document
+      .querySelector(".settings_hidden")
+      .classList.remove("content_settings_visibile");
+  }
+});
+*/
+
+closeButton.addEventListener("click", function () {
+  const element = document.querySelector(".sidebar");
+  const selectors = element.classList;
+  if (selectors.contains("sidebar_closed")) {
+    element.classList.remove("sidebar_closed");
+    element.classList.add("sidebar_visible");
+    document
+      .querySelector(".settings_hidden")
+      .classList.add("content_settings_visibile");
+  } else {
+    element.classList.remove("sidebar_visible");
+    element.classList.add("sidebar_closed");
+    document
+      .querySelector(".settings_hidden")
+      .classList.remove("content_settings_visibile");
+  }
+});
