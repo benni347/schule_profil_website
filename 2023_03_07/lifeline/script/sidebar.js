d9d425dd15e9af6fd004834c3ed986e582ca5d06
Cédric Skwar <cdrc@skwar.me>
2023-03-18 12:40:58 +0100

    Added the rotating menu.

--- 2023_03_07/lifeline/script/sidebar.js
+++ 2023_03_07/lifeline/script/sidebar.js
@@ -1,39 +1,7 @@
-const closeButton = document.querySelector(".close_button");
+const open = document.getElementById("open");
+const close = document.getElementById("close");
+const container = document.querySelector(".container");
 
-/*
-document.querySelector(".settings_cog").addEventListener("click", function () {
-  const element = document.querySelector(".sidebar");
-  const selectors = element.classList;
-  if (selectors.contains("sidebar_closed")) {
-    element.classList.remove("sidebar_closed");
-    element.classList.add("sidebar_visible");
-    document
-      .querySelector(".settings_hidden")
-      .classList.add("content_settings_visibile");
-  } else {
-    element.classList.remove("sidebar_visible");
-    element.classList.add("sidebar_closed");
-    document
-      .querySelector(".settings_hidden")
-      .classList.remove("content_settings_visibile");
-  }
-});
-*/
+open.addEventListener("click", () => container.classList.add("show-nav"));
 
-closeButton.addEventListener("click", function () {
-  const element = document.querySelector(".sidebar");
-  const selectors = element.classList;
-  if (selectors.contains("sidebar_closed")) {
-    element.classList.remove("sidebar_closed");
-    element.classList.add("sidebar_visible");
-    document
-      .querySelector(".settings_hidden")
-      .classList.add("content_settings_visibile");
-  } else {
-    element.classList.remove("sidebar_visible");
-    element.classList.add("sidebar_closed");
-    document
-      .querySelector(".settings_hidden")
-      .classList.remove("content_settings_visibile");
-  }
-});
+close.addEventListener("click", () => container.classList.remove("show-nav"));
