de93bd2d6f0935149e34c11addb01d809fc9f3f8
Cédric Skwar <cdrc@skwar.me>
2023-03-19 10:27:29 +0100

    The sidebar should close when a sidebar element is pressed.

--- 2023_03_07/lifeline/script/sidebar.js
+++ 2023_03_07/lifeline/script/sidebar.js
@@ -1,7 +1,11 @@
-const open = document.getElementById("open");
-const close = document.getElementById("close");
-const container = document.querySelector(".container");
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+const sideBarElements = document.querySelector(".sideBarElements")
+const container = document.querySelector(".container")
 
-open.addEventListener("click", () => container.classList.add("show-nav"));
+open.addEventListener("click", () => container.classList.add("show-nav"))
 
-close.addEventListener("click", () => container.classList.remove("show-nav"));
+close.addEventListener("click", () => container.classList.remove("show-nav"))
+sideBarElements.addEventListener("click", () =>
+    container.classList.remove("show-nav")
+)
