38368b02aea29d2321103924100e17a62706560d
Cédric Skwar <cdrc@skwar.me>
2023-03-19 23:16:11 +0100

    Added a whitebar

--- 2023_03_07/lifeline/script/typing.js
+++ 2023_03_07/lifeline/script/typing.js
@@ -2,10 +2,10 @@ const typingElement = document.querySelector(".typing-animation");
 const text = typingElement.getAttribute("data-text");
 let index = 0;
 const intervalId = setInterval(() => {
-  typingElement.textContent += text[index];
+  typingElement.innerHTML = text.substr(0, index) +
+    '<span class="cursor"></span>';
   index++;
   if (index >= text.length) {
     clearInterval(intervalId);
   }
-}, 100);
-
+}, 200);
