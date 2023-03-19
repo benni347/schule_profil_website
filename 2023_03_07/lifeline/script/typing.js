38e77e872f7d03418e86f3ef699f42c303f0ede3
Cédric Skwar <cdrc@skwar.me>
2023-03-19 23:17:09 +0100

    Added a todo and a fixme

--- 2023_03_07/lifeline/script/typing.js
+++ 2023_03_07/lifeline/script/typing.js
@@ -1,11 +1,15 @@
-const typingElement = document.querySelector(".typing-animation");
-const text = typingElement.getAttribute("data-text");
-let index = 0;
+const typingElement = document.querySelector(".typing-animation")
+const text = typingElement.getAttribute("data-text")
+let index = 0
 const intervalId = setInterval(() => {
-  typingElement.innerHTML = text.substr(0, index) +
-    '<span class="cursor"></span>';
-  index++;
-  if (index >= text.length) {
-    clearInterval(intervalId);
-  }
-}, 200);
+    typingElement.innerHTML =
+        text.substr(0, index) + '<span class="cursor"></span>'
+    index++
+    if (index >= text.length) {
+        clearInterval(intervalId)
+    }
+}, 200)
+/*
+ * FIXME: The Cursor doesn't blink in css
+ * TODO: Remove the cursor when the text is fully shown.
+ */
